3a9c29caa7e0e3c0482b2685047a7e5a4b82a03c
Yosua Winata <yosua.winata@gmail.com>
2024-01-31 14:54:00 +0700

    add events & methods and working with arguments

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ https://unpkg.com/vue@next to https://unpkg.com/vue@3.4.9/dist/vue.global.js
 Adding styles.css, index.html and app.js to create the first application.
 In this chapter, we will learn about create and connect Vue App instances, interpolation and data binding,
 binding attributes with the v-bind directive, methods, working with data inside (using $this) and usage of v-html for outputting rawHtml content.
+
+
+# chapterTwo
+Update styles.css, index.html and app.js from the chapterOne. 
+In this chapter, we will learn about strong of JS, namely events. We will learn also about methods, working with arguments,
+using the native object, explore more event modifiers, and locking content with v-once.
--- app.js
+++ app.js
@@ -1,27 +1,9 @@
 const app = Vue.createApp({
     data() {
-        return {
-            courseGoal: 'Finish the course and learn VueJs!', // the first example use data property
-            courseGoalA: '<h2>Finish the course and learn VueJs!</h2>',
-            courseGoalB: 'Master VueJs and build amazing application!',
-            vueLink: 'https://vuejs.org/'
-        };
+      return {
+        counter: 0,
+      };
     },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                // instead hardcode we can use the variable inside using (this) from data property
-                // return 'Learn VueJs!';
-                return this.courseGoalA;
-            }
-            else {
-                // instead hardcode we can use the variable inside using (this) from data property
-                // return 'Master VueJS!';
-                return this.courseGoalB;
-            }
-        }
-    }
-});
-
-app.mount('#user-goal');
+  });
+  
+  app.mount('#events');  

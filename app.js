4f09c7f59f8d303dbce915a636555874143a9e60
Yosua Winata <yosua.winata@gmail.com>
2024-02-01 11:21:03 +0700

    “test

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const app = Vue.createApp({
     data() {
       return {
         counter: 0,
+        counterMethods: 0,
+        counterMethodsParam: 0
       };
     },
+    methods: {
+        addMethods() {
+            this.counterMethods++; // you can write like this this.counter = this.counter + 1
+        },
+        substractMethods() {
+            this.counterMethods--; // you can write like this this.counterMethods = this.counterMethods - 1
+        },
+        addWith(param) {
+            this.counterMethodsParam = this.counterMethodsParam + param
+        },
+        substractWith(param) {
+            this.counterMethodsParam = this.counterMethodsParam - param
+        }
+    }
   });
   
   app.mount('#events');  
